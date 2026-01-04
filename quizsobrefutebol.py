2c6ad955d7ae04319b9485f1e1e980eefdc6dc9d
Danilo F <felipedaniloss00@gmail.com>
2026-01-04 18:37:59 -0300

    Update quizsobrefutebol.py

--- quizsobrefutebol.py
+++ quizsobrefutebol.py
@@ -11,37 +11,37 @@ print("Qual time mais vezes venceu a Champions League? \n (A) Real Madrid \n (B)
 answer_1 = input("Resposta: ")
 
 if answer_1 == "A":
-    print("Correto!")
+    print("\033[32mACERTOU!\033[0m")
     score = score + 1
 else:
-    print("Incorreto!")
+    print("\033[31mErrou!\033[0m")
 
-print("Qual destes jogadores mais venceu a Champion League? \n (A) Modric \n (B) CR7 \n (C) Messi \n") 
+print("Qual destes jogadores mais venceu a Champion League? \n (A) Modric \n (B) CR7 \n (C) Messi \n")
 answer_2 = input("Resposta: ")
 
 if answer_2 == "A":
-    print("Correto!")
+    print("\033[32mACERTOU!\033[0m")
     score = score + 1
 else:
-    print("Incorreto!")
+    print("\033[31mErrou!\033[0m")
 
 print("Qual seleção tem mais copas do mundo? \n (A) Alemanha \n (B) França \n (C) Brasil \n")
 answer_3 = input("Resposta: ")
 
 if answer_3 == "C":
-    print("Correto!")
+    print("\033[32mACERTOU!\033[0m")
     score = score + 1
 else:
-    print("Incorreto!")
+    print("\033[31mErrou!\033[0m")
 
 print("Qual o jogador com mais copas do mundo conquistadas? \n (A) Neuer \n (B) Pelé \n (C) Maradona \n")
 answer_4 = input("Resposta: ")
 
 if answer_4 == "B":
-    print("Correto!")
+    print("\033[32mACERTOU!\033[0m")
     score = score + 1
 else:
-    print("Incorreto!")
+    print("\033[31mErrou!\033[0m")
 
 print(f"Fim do Quiz... Pontuação: {score}/4")
 
